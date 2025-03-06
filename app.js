42f44e66f070aa760373c63d663b093679002034
newage9999 <99876419+newage9999@users.noreply.github.com>
2025-03-06 15:23:43 +0100

    Moved // ...

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const http = require("http");
 
 function f(req, res) {
   res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+
+  // ...
+
 }
-// ...
+
 http.createServer(f).listen(8081);
 console.log("Server is listening");
